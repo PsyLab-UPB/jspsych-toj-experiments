PsyLab-UPB/jspsych-toj-experiments

commit 59ec9fd3b35d25fea338ea27c706c20951b45de2

change: let participants finish tutorial irrespective of the ratio of correct answers

src/color-toj-negation-07.js
@@ -502,16 +502,14 @@ export async function run({ assetPaths }) {
       cursor_off,
       {
         conditional_function: () =>
-          globalProps.isFirstParticipation &&
-          correctResponsesTutorial < correctResponsesLimitTutorial,
+          globalProps.isFirstParticipation,
         timeline: [toj],
         timeline_variables: trialsTutorial,
         play_feedback: true,
       },
       {
         conditional_function: () =>
-          !globalProps.isFirstParticipation &&
-          correctResponsesTutorial < correctResponsesLimitRepeatedTutorial,
+          !globalProps.isFirstParticipation,
         timeline: [toj],
         timeline_variables: trialsRepeatedTutorial,
         play_feedback: true,

commit bf10610c920b316ff37b931a43535fb8674e0219

fix regression from "change: let participants finish tutorial irrespective of the ratio of correct answers"
fix regression introduced in commit 59ec9fd3b35d25fea338ea27c706c20951b45de2

src/color-toj-negation-07.js
@@ -521,14 +521,16 @@ export async function run({ assetPaths }) {
       cursor_off,
       {
         conditional_function: () =>
-          globalProps.isFirstParticipation,
+          globalProps.isFirstParticipation &&
+          !tutorialAlreadyCompleted,
         timeline: [toj],
         timeline_variables: trialsTutorial,
         play_feedback: true,
       },
       {
         conditional_function: () =>
-          !globalProps.isFirstParticipation,
+          !globalProps.isFirstParticipation &&
+          !tutorialAlreadyCompleted,
         timeline: [toj],
         timeline_variables: trialsRepeatedTutorial,
         play_feedback: true,